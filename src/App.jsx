d5fb842df09c11157da46db55bcbc440068ef774
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-10 16:55:27 -0600

    Setup name password and submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <form>
+      <label htmlFor="name" className="sr-only">
+        Name
+      </label>
+      <input type="text" id="name" name="name" placeholder="Name" />
+      <label htmlFor="password" className="sr-only">
+        Password{" "}
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Password"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default App;
