162f6e85f292841c1ef0e746e56af4291fd9b823
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-11 10:37:53 -0600

    Create form folder and add new files input-password.jsx and input-text.jsx

---
+++ src/components/form/input-text.jsx
@@ -0,0 +1,15 @@
+export default function InputText() {
+  return (
+    <label htmlFor="name" className="sr-only">
+        Name
+      </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Name"
+        className="rounded"
+      />
+  )
+}
+   
