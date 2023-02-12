d5849ed1b3a267760f7fddb74a62a6953896dad5
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-12 16:27:24 -0600

    Create file and copy paste code

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import ky from "ky";
+
+const BASE_URL = "http://localhost:3000/api";
+
+export default {
+  loginOrRegister(credentials, isRegistering) {
+    return ky
+      .post(`${BASE_URL}/users/${isRegistering ? "create" : "login"}`, {
+        json: credentials,
+      })
+      .json();
+  },
+  getStudents() {
+    return ky.get(`${BASE_URL}/students`).json();
+  },
+  createStudent(data) {
+    return ky.post(`${BASE_URL}/students`, { json: data }).json();
+  },
+};
