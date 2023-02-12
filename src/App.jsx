1db763ff3cdb9d4b985664a7bfecd5db20853ced
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-12 16:22:22 -0600

    Copy paste code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import InputText from "./components/form/input-text";
-import Button from "./components/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginRegister from "./routes/login-register";
+import Students from "./routes/students";
+import { apiService } from "./services";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginRegister />,
+  },
+
+  // TODO: Protect this route (check for a jwt)
+  {
+    path: "/students",
+    element: <Students />,
+    loader() {
+      return apiService.getStudents();
+    },
+
+    async action(postSubmission) {
+      const { request } = postSubmission;
+
+      // Read the request body as form data
+      const formData = await request.formData();
+
+      // Turn the form data into a plain object
+      const data = Object.fromEntries(formData);
+
+      // Send the data to the server
+      const newStudent = await apiService.createStudent(data);
+
+      return { newStudent };
+    },
+  },
+]);
 
 function App() {
-  return (
-    <form className="mt-4 flex flex-col items-center gap-y-2">
-      <InputText label="Name" id="name" />
-
-      <InputText label="Password" id="password" type="password" />
-      <InputText
-        label="Confirm Password"
-        id="confirmPassword"
-        type="password"
-      />
-
-      <Button text="Enter" />
-    </form>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
