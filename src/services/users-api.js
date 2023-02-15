3db326fcd5484d37a9af7f0c98a177fa29f8e8bb
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-15 13:15:50 -0600

    Create users-api.js and code

---
+++ src/services/users-api.js
@@ -0,0 +1,31 @@
+import ky from "ky";
+
+const { VITE_BASE_URL } = import.meta.env;
+
+export default {
+  index() {
+    return ky
+      .post(`${VITE_BASE_URL}/users/super`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .json();
+  },
+  signIn({ username, password }) {
+    return ky
+      .post(`${VITE_BASE_URL}/users/${isRegistering ? "register" : "login"}`, {
+        json: { username, password },
+      })
+      .json();
+  },
+  destroy(id) {
+    return ky
+      .delete(`${VITE_BASE_URL}/users/super/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .json();
+  },
+};
