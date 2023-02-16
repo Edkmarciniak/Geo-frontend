6c0f816559c90b4c3e672b726bfec126ddb7cbee
Ed Marciniak <ekmarciniak@gmail.com>
2023-02-15 18:16:01 -0600

    Import axios and return

--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -1,47 +1,82 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
-  return (
-    <form className="mt-4 flex flex-col items-center gap-y-2">
-      <label htmlFor="username" className="sr-only">
-        Username
-      </label>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        placeholder="Username"
-        className="rounded"
-      />
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-      <label htmlFor="password" className="sr-only">
-        Password
-      </label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Password"
-        className="rounded"
-      />
+  // const changeHandler = (e) => {
+  //   setUser({ ...user, [e.target.name]: e.target.value });
+  // };
+  const submitHandler = () => {
+    axios.post("http://localhost:3000/user/signIn", user).then((res) => {
+      console.log(res.data);
+      setUser({ email: "Sansa", password: "" });
+    });
+  };
 
-      <label htmlFor="confirmPassword" className="sr-only">
-        Confirm Password
-      </label>
-      <input
-        type="password"
-        id="confirmPassword"
-        placeholder="Confirm Password"
-        className="rounded"
-      />
+  const renderStuff = () => {
+    if (user.email.length > 0) {
+      return (
+        <div className="mt-4 flex flex-col items-center gap-y-2">
+          Hello {user.email}
+        </div>
+      );
+    } else {
+      return (
+        <div className="mt-4 flex flex-col items-center gap-y-2">
+          <label htmlFor="username" className="sr-only">
+            Username
+          </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Email"
+            className="rounded"
+            // onChange={changeHandler}
+            // value={user.email}
+          />
 
-      {/* TODO: Toggle between Register and Login */}
-      <button
-        type="submit"
-        className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-      >
-        Enter
-      </button>
-    </form>
-  );
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            className="rounded"
+            // onChange={changeHandler}
+            // value={user.password}
+          />
+
+          <label htmlFor="confirmPassword" className="sr-only">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm Password"
+            className="rounded"
+          />
+
+          {/* TODO: Toggle between Register and Login */}
+          <button
+            type="submit"
+            className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+            onClick={submitHandler}
+          >
+            Enter
+          </button>
+        </div>
+      );
+    }
+  };
+
+  return renderStuff();
 }
 
 export default App;
